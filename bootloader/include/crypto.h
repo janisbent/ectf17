7d412ca74704b40bd2db4939af527c95e3ca0fad
Toby Glover <william.glover@tufts.edu>
2017-02-19 11:07:38 -0500

    changed to reflect new frame size

--- bootloader/include/crypto.h
+++ bootloader/include/crypto.h
@@ -9,7 +9,7 @@
 #include <avr/wdt.h>
 
 /*
- * Takes a 1024B frame in i_data, decrypts and verifies authenticity, and 
+ * Takes a 512B frame in i_data, decrypts and verifies authenticity, and 
  * adds the decrypted data to the buffer starting at buffer[buffer_start]
  *
  * Returns the size of the data added to buffer or -1 on failed verification
@@ -18,8 +18,8 @@ int decrypt_frame(unsigned char *frame, unsigned char *buffer,
                   unsigned int buffer_start);
 
 /*
- * Accepts data of size bytes in buffer (must be under 470B) and places
- * encrypted 512B frame into frame
+ * Accepts data of size bytes in buffer (must be under 214B) and places
+ * encrypted 256B frame into frame
  */
 void encrypt_frame(unsigned char *frame, unsigned char *buffer,
                    unsigned int size);
