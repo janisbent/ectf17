4d9a4ecc6fe4b22d9ae159b24250bcdbe6b0010c
Ben Janis <janisbent@gmail.com>
2017-02-20 07:44:43 +0000

    Fixed yes.py

--- bootloader/tests/yes.py
+++ bootloader/tests/yes.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 
 import serial
+import argparse
+
+parser = argparse.ArgumentParser(description='Bootloader Config Tool')
+parser.add_argument('--port', help='Serial port to use for configuration.',
+                    required=True)
+args = parser.parse_args()
 
 ser = serial.Serial(args.port, baudrate=115200, timeout=2) 
 
