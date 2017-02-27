9fbd10bce770271cf1b4ecdaf9d938653fc39a8a
mmorov01 <mmorovitz@mitre.org>
2017-02-27 10:06:57 -0500

    new crypto interface

--- bootloader/include/crypto.h
+++ bootloader/include/crypto.h
@@ -1,25 +1,26 @@
 
 #ifndef DECRYPT_H
 #define DECRYPT_H
-
-#define FRAME_SIZE 512
-
-/*
- * Takes a 512B frame in i_data, decrypts and verifies authenticity, and 
- * adds the decrypted data to the buffer starting at buffer[buffer_start]
- *
- * Returns the size of the data added to buffer or -1 on failed verification
- */
-int decrypt_frame(unsigned char *frame, unsigned char *buffer, 
-                  unsigned int buffer_start);
-
-/*
- * Accepts data of size bytes in buffer (must be under 214B) and places
- * encrypted 512B frame into frame. Returns size of encrypted data.
- */
-int encrypt_frame(unsigned char *frame, unsigned char *buffer,
-                   unsigned int size);
+#include <stdbool.h>
 
 
+/* 
+* Message format:
+* 
+* Components: SHA256 Hash | DSA Signed AES Key | RSA Encrypted AES Key | AES Encrypted Binary |
+* Size      :    32B      |       320b         |        256B           |       (variable)     |
+* 
+*/
+
+// return true if hash of data is correct
+// changes buffer start to be one index past hash location at return
+bool verify_hash(char* buffer, int* buffer_start);
+
+// places decrypted message in dec_msg
+// returns size of encrypted message, -1 if DSA signature fails
+int decrypt(char* buffer, int buffer_start, char* dec_msg);
+
+//returns size of encrypted message, places message in enc_msg
+int encrypt(char* buffer, int buffer_start, char* enc_msg);
 
 #endif
