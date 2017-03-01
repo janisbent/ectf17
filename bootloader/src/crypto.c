e3ce6d3af483e669c8ed74e3152a54059bae243e
Ubuntu <ubuntu@mitre-ectf-vm>
2017-03-01 22:55:24 +0000

    Implements an AES encryption interface

--- bootloader/src/crypto.c
+++ bootloader/src/crypto.c
@@ -9,8 +9,38 @@
 #include <avr/interrupt.h>
 #include <avr/wdt.h>
 
-/* Assumes the buffer length is 16 bytes */ 
-void decrypt(uint8_t *enc_data, uint8_t* dec_data)
+#define BUFF_LEN 16
+const unsigned char KEY[] = "1234567890123456";
+
+/* Assumes the buffer's length is 16 bytes */ 
+void decrypt(uint8_t *enc_data, uint8_t *dec_data)
+{
+    /* Debugging */ 
+    for (int i = 0; i < BUFF_LEN; i++) {
+        UART1_putchar(enc_data[i]);
+    }
+
+    AES128_ECB_decrypt(enc_data, KEY, dec_data);
+
+    /* Debugging */
+    for (int i = 0; i < BUFF_LEN; i++) {
+        UART1_putchar(dec_data[i]);
+    }
+}
 
 /* Assumes the buffer's length is 16 bytes */
-void encrypt(uint8_t *dec_data, uint8_t* enc_data)
+void encrypt(uint8_t *dec_data, uint8_t *enc_data),
+{
+
+    /* Debugging */
+    for (int i = 0; i < BUFF_LEN; i++) {
+        UART1_putchar(dec_data[i]);
+    }
+
+    AES128_ECB_encrypt(dec_data, KEY, enc_data); 
+
+    /* Debugging */
+    for (int i = 0; i < BUFF_LEN; i++) {
+        UART1_putchar(enc_data[i]);
+    }
+}
