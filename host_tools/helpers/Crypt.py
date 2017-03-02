5078b72aa8f45522053d9fea12e7d45ddb484bc1
Toby Glover <william.glover@tufts.edu>
2017-03-02 01:59:00 -0500

    crypto for signing

--- host_tools/helpers/Crypt.py
+++ host_tools/helpers/Crypt.py
@@ -8,42 +8,67 @@ for both factory and bootloader
 
 from SecretFile import SecretFile
 from Crypto.Cipher import AES
+from Crypto.PublicKey import DSA
 from Crypto.Random import get_random_bytes
 from Crypto.Hash import SHA256
+from Crypto.Signature import DSS
 
 FRAME_SIZE = 16
+
+PUBLIC = 1
+PRIVATE = 0
 
 class Crypt:
 
 	def __init__(self, directory):
-		sf = SecretFile(directory)
+		self.sf = SecretFile(directory)
 
-		self.key = "1234567890123456"
+	def __del__(self):
+		self.sf.flush()
 
-		# self.key = sf.getKey()
-		# if self.key is None:
-		# 	self.key = get_random_bytes(16)
-		# 	sf.setKey(self.key);
-		# 	sf.flush()
+	def getAESKey(self):
+		key = self.sf.getKey(self.sf.AES_KEY)
+		if key is None:
+			key = get_random_bytes(16)
+			self.sf.setKey(self.sf.AES_KEY, key)
 
-	def getKey(self):
-		return self.key
+		return key
+
+	def getDSAKey(self, part=PUBLIC):
+		key = self.sf.getKey(self.sf.DSA_KEY)
+		if key is None:
+			key = DSA.generate(1024)
+			self.sf.setKey(self.sf.DSA_KEY, key.exportKey())
+		else:
+			key = DSA.import_key(key)
+
+		if part == PUBLIC:
+			return key.publickey()
+
+		return key
 
 	def hash(self, msg):
 		return SHA256.new(msg).digest()
 
-	# http://legrandin.github.io/pycryptodome/Doc/3.4/Crypto.Cipher.AES-module.html
+	def sign(self, msg):
+		key = self.getDSAKey(part=PRIVATE)
+		hashMsg = SHA256.new(msg)
+		signer = DSS.new(key, 'fips-186-3')
+		return signer.sign(hashMsg)
+
 	def encode(self, msg):
-		cipher = AES.new(self.key, AES.MODE_ECB)
+		key = self.getAESKey()
+		cipher = AES.new(key, AES.MODE_ECB)
 		msg = self.randomPadToSize(msg)
 		return cipher.encrypt(msg)
 
 	def decode(self, msg):
-		cipher = AES.new(self.key, AES.MODE_ECB)
+		key = self.getAESKey()
+		cipher = AES.new(key, AES.MODE_ECB)
 		return cipher.decrypt(msg)
 
 	def randomPadToSize(self, msg, size=FRAME_SIZE):
-		if len(msg) == size:
+		if len(msg) > size:
 			return msg
 
 		return msg + get_random_bytes(size - len(msg))
--- host_tools/helpers/SecretFile.py
+++ host_tools/helpers/SecretFile.py
@@ -6,29 +6,48 @@ Represents secret_configure_output.txt. Allows for the access and writing of all
 """
 
 import os
+import json
 
 FILE_NAME = "secret_configure_output.txt"
 
 class SecretFile:
 
+	AES_KEY = "AES"
+	DSA_KEY = "DSA"
+
 	def __init__(self, directory):
 		self.filePath = os.path.join(directory, FILE_NAME)
-		self.key = None
+		self.keys = dict()
 		self.__readDataFromFile()
 
-	def setKey(self, value):
-		self.key = value
+	def setKey(self, key, value):
+		if self.__isValidKey(key):
+			if key == self.AES_KEY:
+				self.keys[key] = value.encode("hex")
+			else:
+				self.keys[key] = value
 
-	def getKey(self):
-		return self.key
+	def getKey(self, key):
+		if self.__isValidKey(key) and key in self.keys:
+			value = self.keys[key]
+
+			if key == self.AES_KEY:
+				value = value.decode("hex")
+
+			return value
+		else:
+			return None
 
 	def flush(self):
 		with open(self.filePath, 'w+') as file:
-			file.write(self.key)
+			file.write(json.dumps(self.keys))
+
+	def __isValidKey(self, key):
+		return key == self.AES_KEY or key == self.DSA_KEY
 
 	def __readDataFromFile(self):
 		if os.path.isfile(self.filePath):
 			with open(self.filePath, 'r') as file:
-				strData = file.read()
-
-			self.key = strData
+				fileStr = file.read()
+				if len(fileStr) > 0:
+					self.keys = json.loads(fileStr)
