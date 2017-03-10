7ba4a98e90256757ed55cc456b5f5e55bb1d8a3c
Ubuntu <ubuntu@mitre-ectf-vm>
2017-03-10 08:49:04 +0000

    dsa_verify consumes too much memory (again)

--- bootloader/src/verify.c
+++ bootloader/src/verify.c
@@ -1,25 +1,49 @@
-#include "dsa_verify.h"
-#include "verify.h"
+#include <stdint.h>
+#include <stdbool.h>
+#include <avr/pgmspace.h>
 
-#define SHA_LEN 256
+#include "dsa_verify.h"
+#include "sha1.h"
+#include "uart.h"
+#include "verify.h"
+// #include "keys.h"
+
+// The length of a sha1 hash is 160 bits, or 20 bytes
+
+#define SHA_LEN_BYTES 20
+#define FRAME_BYTES 16
+
+/* TEMPORARY HARD-CODED DSA KEY TO SILENCE COMPILER WARNINGS
+ * REMOVE AT A LATER DATE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! 
+ */
+const unsigned char *DSA_KEY = (unsigned char*) "1234567812345678";
 
 //returns 0 on sucess and 1 on fail
 int dsa_verify(uint8_t* message, uint8_t* R, uint8_t* S){
-   return dsa_verify_blob(message, SHA_LEN, DSA_KEY, R, S);
+   return dsa_verify_blob((char *)message, SHA_LEN_BYTES, DSA_KEY, (char *)R, (char *)S);
+   // Never reached
+   return -1;
 }
 
-bool verify_hash(uint8_t* buffer, int buffer_start){
-   uint8_t hash[SHA256_HASH_BYTES];
-   uint8_t data[SHA_LEN];
-   uint8_t orig_hash[SHA256_HASH_BYTES];
+bool verify_hash(uint8_t* buffer, int buffer_start) {
+   // Declare and initialize the SHA1 context   
+   SHA1Context *context = 0x0;
+   SHA1Reset(context);
+
+   uint8_t hash[SHA_LEN_BYTES];
+   uint8_t precomp_hash[SHA_LEN_BYTES];
+   uint8_t data[FRAME_BYTES];
+   
    int sum = 0;   
 
-   memcpy_P(data, buffer+SHA256_HASH_BYTES, SHA_LEN);
-   memcpy_P(orig_hash, buffer, SHA256_HASH_BYTES);
-   sha256((sha256_hash_t*)hash, data, SHA_LEN * 8);
+   memcpy_P(precomp_hash, buffer, SHA_LEN_BYTES);
+   memcpy_P(data, buffer + SHA_LEN_BYTES, FRAME_BYTES);
+
+   SHA1Input(context, data, FRAME_BYTES);
+   SHA1Result(context, hash);
    
-   for(int i = 0; i < SHA_LEN; i++){
-         sum = sum + ((hash + i) ^ (orig_hash + i));
+   for (int i = 0; i < SHA_LEN_BYTES; i++) {
+         sum = sum + ((*(hash + i)) ^ (*(precomp_hash + i)));
    }
 
    return !sum;
